b99d7fc9c8af392a46ab7624a8fd94ec240bf926
Purvesh <purveshkolhe27@gmail.com>
2026-06-02 12:02:05 +0530

    almost done

--- 03/main.js
+++ 03/main.js
@@ -1,4 +1,15 @@
-let myvar = getElementById("logo");
+let myvar = document.getElementById("logo");
 myvar.addEventListener("click", function() {
-    console.log("You clicked the logo!");
+    alert("You clicked the logo!");
+});
+let mylist = document.getElementsByClassName("tabs");
+for (let i = 0; i < mylist.length; i++) {
+    mylist[i].addEventListener("click", function() {
+        document.getElementsByClassName('unavail')[0].style.display = 'block';
+        document.getElementsByClassName('backdrop')[0].style.display = 'block';
+    });
+}
+document.getElementsByClassName('cross')[0].addEventListener("click", function() {
+    document.getElementsByClassName('unavail')[0].style.display = 'none';
+    document.getElementsByClassName('backdrop')[0].style.display = 'none';
 });
